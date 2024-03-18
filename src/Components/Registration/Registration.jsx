3918a949ecf0909306c4321560065e337b3d6b9e
Tofazzal26 <afranislamabir6789@gmail.com>
2024-03-18 10:30:37 +0600

    email password name validation finish

--- src/Components/Registration/Registration.jsx
+++ src/Components/Registration/Registration.jsx
@@ -4,6 +4,7 @@ const Registration = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleClick = (event) => {
     event.preventDefault();
@@ -12,7 +13,7 @@ const Registration = () => {
       email: email,
       password: password,
     };
-    console.log(user);
+    setIsSubmit(true);
   };
 
   return (
@@ -26,6 +27,7 @@ const Registration = () => {
             <h4 className="text-lg font-semibold mb-1">Name</h4>
             <label className="input input-bordered flex items-center gap-2">
               <input
+                required
                 onChange={(event) => setName(event.target.value)}
                 type="text"
                 className="grow text-base text-gray-500 font-semibold"
@@ -36,6 +38,7 @@ const Registration = () => {
             <h4 className="text-lg font-semibold mb-1">Email</h4>
             <label className="input input-bordered flex items-center gap-2">
               <input
+                required
                 onChange={(event) => setEmail(event.target.value)}
                 type="email"
                 className="grow text-base text-gray-500 font-semibold"
@@ -45,6 +48,7 @@ const Registration = () => {
             <h4 className="text-lg font-semibold mb-1">Password</h4>
             <label className="input input-bordered flex items-center gap-2">
               <input
+                required
                 onChange={(event) => setPassword(event.target.value)}
                 type="password"
                 className="grow text-base text-gray-500 font-semibold"
@@ -60,6 +64,18 @@ const Registration = () => {
           </form>
         </div>
       </div>
+      {isSubmit && (
+        <div className="fixed top-0 h-screen w-full flex justify-center bg-purple-500 items-center">
+          <div className="bg-purple-300 w-1/2 h-2/3 rounded text-center">
+            <h2 className="text-3xl mt-3 mb-6 text-purple-500">
+              You have submitted the form successfully !
+            </h2>
+            <p className="text-lg font-semibold">Name: {name}</p>
+            <p className="text-lg font-semibold">Email: {email}</p>
+            <p className="text-lg font-semibold">Password: {password}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
